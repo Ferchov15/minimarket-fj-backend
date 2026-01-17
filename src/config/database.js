ac0c59d31708d9ce3a6785bca42f075ebd1df95a
Fernando Daniel Vaca Buitrón <fdvaca@puce.edu.ec>
2026-01-16 23:42:18 -0500

    Correción de errores de despliegue

--- src/config/database.js
+++ src/config/database.js
@@ -3,15 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT || 5432,
-    logging: false, 
+    logging: false,
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
   }
 );
 
